51fe03de723f356fe89f62dc147e6b4042dd662b
NakagamiYuta <guguru0014@gmail.com>
2020-03-04 20:56:49 +0900

    [add] mainからParser.parseを呼ぶようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use dragonbook::chapter2::infix2suffix::Parser;
+
 fn main() {
-    println!("DragonBook -Compiler-");
+    let mut parser = Parser::new("1-2+0-4".to_string());
+    parser.parse();
+    println!();
 }
