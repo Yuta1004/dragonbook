963aa3eee38d1e3d6f58f1fc34faa0e0a733b061
NakagamiYuta <guguru0014@gmail.com>
2020-03-04 21:42:17 +0900

    [change] 引数で式を受け取るようにした

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::env;
+use std::process::exit;
 use dragonbook::chapter2::infix2suffix::Parser;
 
 fn main() {
-    let mut parser = Parser::new("1-2+0-4".to_string());
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("[usage] cargo run 1-2+0-4");
+        exit(1);
+    }
+    let expr = args[1].to_string();
+
+    let mut parser = Parser::new(expr);
     parser.parse();
     println!();
 }
