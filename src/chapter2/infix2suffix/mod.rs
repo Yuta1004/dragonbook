b3499f2b41a6e95e4d04075d39ebaf275c30df84
NakagamiYuta <guguru0014@gmail.com>
2020-03-04 21:36:01 +0900

    [clean] モジュール整理

--- src/chapter2/infix2suffix/mod.rs
+++ src/chapter2/infix2suffix/mod.rs
@@ -1,3 +1,5 @@
+mod tests;
+
 pub struct Parser {
     expr: String,
     lookidx: i32,
@@ -77,41 +79,3 @@ impl Parser {
         panic!("[ERROR] {} : at {}", msg, self.lookidx);
     }
 }
-
-#[test]
-fn new_test() {
-    let _ = Parser::new("1+1".to_string());
-}
-
-#[test]
-fn parse_test() {
-    let mut parser = Parser::new("9+5-2".to_string());
-    parser.parse();
-}
-
-#[test]
-fn term_test() {
-    let mut parser = Parser::new("2+2".to_string());
-    parser.term();
-}
-
-#[test]
-fn expect_test() {
-    let mut parser = Parser::new("abcdefg".to_string());
-    assert!(parser.expect('a'));
-    assert!(!parser.expect('A'));
-}
-
-#[test]
-fn lookahead_test() {
-    let parser = Parser::new("abcdefg".to_string());
-    assert_eq!('a', parser.get_lookahead());
-}
-
-#[test]
-#[should_panic]
-fn error_test() {
-    let parser = Parser::new("abcdefg".to_string());
-    parser.error("PANIC");
-}
-
---
+++ src/chapter2/infix2suffix/tests.rs
@@ -0,0 +1,38 @@
+use super::super::infix2suffix::Parser;
+
+#[test]
+fn new_test() {
+    let _ = Parser::new("1+1".to_string());
+}
+
+#[test]
+fn parse_test() {
+    let mut parser = Parser::new("9+5-2".to_string());
+    parser.parse();
+}
+
+#[test]
+fn term_test() {
+    let mut parser = Parser::new("2+2".to_string());
+    parser.term();
+}
+
+#[test]
+fn expect_test() {
+    let mut parser = Parser::new("abcdefg".to_string());
+    assert!(parser.expect('a'));
+    assert!(!parser.expect('A'));
+}
+
+#[test]
+fn lookahead_test() {
+    let parser = Parser::new("abcdefg".to_string());
+    assert_eq!('a', parser.get_lookahead());
+}
+
+#[test]
+#[should_panic]
+fn error_test() {
+    let parser = Parser::new("abcdefg".to_string());
+    parser.error("PANIC");
+}
