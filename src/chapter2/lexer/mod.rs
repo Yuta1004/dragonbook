968dd88ec460f7c4eba49062fa072fa6454cfc22
NakagamiYuta <guguru0014@gmail.com>
2020-03-07 22:48:36 +0900

    [add] Tag列挙体定義 & 一部Tag追加

--- src/chapter2/lexer/mod.rs
+++ src/chapter2/lexer/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests;
 
+// Token
 pub struct Token {
     tag: i32,
 }
@@ -9,4 +10,16 @@ impl Token {
     pub fn new(tag: i32) -> Token {
         Token { tag }
     }
+}
+
+// Tag
+pub enum Tag {
+    Plus,
+    Sub,
+    Mul,
+    Div,
+    Num,
+    Id,
+    True,
+    False
 }
