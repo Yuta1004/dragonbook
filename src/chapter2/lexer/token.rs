c29b1cb940418fa34a711925b5212e04fbc9a3c4
NakagamiYuta <guguru0014@gmail.com>
2020-03-12 00:16:24 +0900

    [change] new_num, new_wordをpublicメソッドに

--- src/chapter2/lexer/token.rs
+++ src/chapter2/lexer/token.rs
@@ -18,11 +18,11 @@ pub enum Token {
 }
 
 impl Token {
-    fn new_num(num: i32) -> Token {
+    pub fn new_num(num: i32) -> Token {
         Token::Num { tag: Tag::Num, num }
     }
 
-    fn new_word(tag: Tag, lexeme: String) -> Token {
+    pub fn new_word(tag: Tag, lexeme: String) -> Token {
         Token::Word { tag, lexeme }
     }
 }
