b84b224d25a582fc27fec5985a04ad1105e49176
NakagamiYuta <guguru0014@gmail.com>
2020-03-11 23:57:53 +0900

    [change] Lexerでのプログラム文字列の持ち方を変更

--- src/chapter2/lexer.rs
+++ src/chapter2/lexer.rs
@@ -8,16 +8,26 @@ use token::Token;
 pub struct Lexer<'a> {
     line: i32,
     nowon: i32,
-    program: String,
+    program: Vec<char>,
     matchTable: HashMap<String, &'a Token>
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(program: String) -> Lexer<'a> {
-        Lexer { line: 0, nowon: 0, program, matchTable: HashMap::new() }
+        Lexer {
+            line: 0,
+            nowon: 0,
+            program: program.chars().collect::<Vec<char>>(),
+            matchTable: HashMap::new()
+        }
     }
 
     fn reserve(&mut self, tag: String, token: &'a Token) {
         self.matchTable.insert(tag, token);
     }
+}
+
+#[test]
+fn lexer_easy_test() {
+    let _ = Lexer::new("gochiusa.com".to_string());
 }
