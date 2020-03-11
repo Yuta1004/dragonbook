c86f139d225cdaca177c30dfaa9972af06fd7d66
NakagamiYuta <guguru0014@gmail.com>
2020-03-11 23:42:48 +0900

    [add] reserveメソッド実装

--- src/chapter2/lexer.rs
+++ src/chapter2/lexer.rs
@@ -5,15 +5,19 @@ use std::collections::HashMap;
 use token::Token;
 
 // scanner
-pub struct Lexer {
+pub struct Lexer<'a> {
     line: i32,
     nowon: i32,
     program: String,
-    matchTable: HashMap<String, Token>
+    matchTable: HashMap<String, &'a Token>
 }
 
-impl Lexer {
-    pub fn new(program: String) -> Lexer {
+impl<'a> Lexer<'a> {
+    pub fn new(program: String) -> Lexer<'a> {
         Lexer { line: 0, nowon: 0, program, matchTable: HashMap::new() }
+    }
+
+    fn reserve(&mut self, tag: String, token: &'a Token) {
+        self.matchTable.insert(tag, token);
     }
 }
