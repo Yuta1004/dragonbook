8ce09e1208ee95508b942007bfeb23d2de16362d
NakagamiYuta <guguru0014@gmail.com>
2020-03-11 23:32:18 +0900

    [change] Lexerのメンバに記号票を追加

--- src/chapter2/lexer.rs
+++ src/chapter2/lexer.rs
@@ -1,14 +1,19 @@
 mod token;
 
+use std::collections::HashMap;
+
+use token::Token;
+
 // scanner
 pub struct Lexer {
     line: i32,
     nowon: i32,
-    program: String
+    program: String,
+    matchTable: HashMap<String, Token>
 }
 
 impl Lexer {
     pub fn new(program: String) -> Lexer {
-        Lexer { line: 0, nowon: 0, program }
+        Lexer { line: 0, nowon: 0, program, matchTable: HashMap::new() }
     }
 }
