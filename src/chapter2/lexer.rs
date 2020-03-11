ca2c394ede9eb4c197bc76ae5734047ada1a114c
NakagamiYuta <guguru0014@gmail.com>
2020-03-11 22:40:45 +0900

    [add] Lexer構造体定義

--- src/chapter2/lexer.rs
+++ src/chapter2/lexer.rs
@@ -11,3 +11,16 @@ pub enum Tag {
     True,
     False
 }
+
+// scanner
+pub struct Lexer {
+    line: i32,
+    nowon: i32,
+    program: String
+}
+
+impl Lexer {
+    pub fn new(program: String) -> Lexer {
+        Lexer { line: 0, nowon: 0, program }
+    }
+}
