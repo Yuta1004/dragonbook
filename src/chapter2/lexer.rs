44d423aad2d8d72f85eb238b22cee55ce06611eb
NakagamiYuta <guguru0014@gmail.com>
2020-03-14 23:26:16 +0900

    [fix] Lexer::reserveの挙動を変更

--- src/chapter2/lexer.rs
+++ src/chapter2/lexer.rs
@@ -25,9 +25,9 @@ impl Lexer {
     }
 
     fn reserve(&mut self, token: Token) {
-        match token {
-            Token::Word { tag, lexeme } => {
-                self.match_table.insert(lexeme.clone(), Token::new_word(tag, lexeme));
+        match token.clone() {
+            Token::Word { tag: _, lexeme } => {
+                self.match_table.insert(lexeme.clone(), token);
             },
             _ => {}
         };
