a39c04c91e55992f6ba8cc02f46aadcd1333d96d
NakagamiYuta <guguru0014@gmail.com>
2020-03-18 00:15:07 +0900

    [change] infix2suffixのテストをinfix2suffix.rsに移動

--- src/chapter2/parser/infix2suffix.rs
+++ src/chapter2/parser/infix2suffix.rs
@@ -1,6 +1,3 @@
-#[cfg(test)]
-mod tests;
-
 pub struct Parser {
     expr: String,
     lookidx: i32,
@@ -83,5 +80,47 @@ impl Parser {
             space.push(' ');
         }
         panic!("\n[ERROR] {} : {}\n{}^at here\n", msg, self.expr, space);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::infix2suffix::Parser;
+
+    #[test]
+    fn new_test() {
+        let _ = Parser::new("1+1".to_string());
+    }
+
+    #[test]
+    fn parse_test() {
+        let mut parser = Parser::new("9+5-2".to_string());
+        parser.parse();
+    }
+
+    #[test]
+    fn term_test() {
+        let mut parser = Parser::new("2+2".to_string());
+        parser.term();
+    }
+
+    #[test]
+    fn expect_test() {
+        let mut parser = Parser::new("abcdefg".to_string());
+        assert!(parser.expect('a'));
+        assert!(!parser.expect('A'));
+    }
+
+    #[test]
+    fn lookahead_test() {
+        let parser = Parser::new("abcdefg".to_string());
+        assert_eq!('a', parser.get_lookahead());
+    }
+
+    #[test]
+    #[should_panic]
+    fn error_test() {
+        let parser = Parser::new("abcdefg".to_string());
+        parser.error("PANIC");
     }
 }
