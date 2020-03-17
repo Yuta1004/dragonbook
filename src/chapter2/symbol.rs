6712a1512d5695484e1f10b8a965037cd429ce15
NakagamiYuta <guguru0014@gmail.com>
2020-03-18 00:07:42 +0900

    [add] SymbolTable生成関連の関数を書いた
    - new
    - new_with_table

--- src/chapter2/symbol.rs
+++ src/chapter2/symbol.rs
@@ -37,10 +37,46 @@ pub struct SymbolTable {
     table: HashMap<String, Symbol>
 }
 
+impl SymbolTable {
+    /// rootに位置する記号表を生成して返す
+    ///
+    /// # returns
+    /// - Box<Option<SymbolTable>>
+    pub fn new() -> Box<Option<SymbolTable>> {
+        Self::new_with_table(Box::new(None))
+    }
+
+    /// 親を持った記号表を生成する
+    ///
+    /// # params
+    /// prev: Box<Option<SymbolTable>> => 親となる記号表
+    ///
+    /// # return
+    /// - Box<Option<SymbolTable>>
+    pub fn new_with_table(prev: Box<Option<SymbolTable>>) -> Box<Option<SymbolTable>> {
+        Box::new(
+            Some(
+                SymbolTable { prev, table: HashMap::new() }
+            )
+        )
+    }
+}
+
 #[cfg(test)]
-#[test]
-fn symbol_new_test() {
-    let lexeme = "abcdefghijklmn".to_string();
-    let ty = Type::new_i32();
-    let _ = Symbol::new(lexeme, ty);
+mod tests {
+    use super::{Symbol, SymbolTable};
+    use super::super::mtype::Type;
+
+    #[test]
+    fn symbol_new_test() {
+        let lexeme = "abcdefghijklmn".to_string();
+        let ty = Type::new_i32();
+        let _ = Symbol::new(lexeme, ty);
+    }
+
+    #[test]
+    fn symbol_table_new_test() {
+        let root = SymbolTable::new();
+        let _ = SymbolTable::new_with_table(root);
+    }
 }
