460517a979c1626a3408027bb51863f7b8b359b9
NakagamiYuta <guguru0014@gmail.com>
2020-03-17 23:35:18 +0900

    [add] Symbol構造体実装
    - newなど

--- src/chapter2/symbol.rs
+++ src/chapter2/symbol.rs
@@ -1,2 +1,34 @@
+use super::mtype::Type;
+
+/// 記号表で管理する1単位を表す
+///
+/// # members
+/// - lexeme: String => 語
+/// - ty: Type => 型
 #[derive(PartialEq)]
-pub struct Symbol {}
+pub struct Symbol {
+    lexeme: String,
+    ty: Type
+}
+
+impl Symbol {
+    /// Symbol構造体を生成して返す
+    ///
+    /// # params
+    /// - lexeme: String => 語
+    /// - ty: Type => 型
+    ///
+    /// # return
+    /// - Symbol
+    pub fn new(lexeme: String, ty: Type) -> Symbol {
+        Symbol { lexeme, ty }
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn symbol_new_test() {
+    let lexeme = "abcdefghijklmn".to_string();
+    let ty = Type::new_i32();
+    let _ = Symbol::new(lexeme, ty);
+}
