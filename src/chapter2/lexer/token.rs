6a41203a18bc768b90b90f0a2c17ea3a2631b12e
NakagamiYuta <guguru0014@gmail.com>
2020-03-17 22:42:10 +0900

    [add] Token, Tagにコメントを追加

--- src/chapter2/lexer/token.rs
+++ src/chapter2/lexer/token.rs
@@ -1,6 +1,11 @@
 use std::clone::Clone;
 use std::str::FromStr;
 
+/// Tag
+///
+/// # derive
+/// - Clone
+/// - PartialEQ
 #[derive(Clone, PartialEq)]
 pub enum Tag {
     UpperThanL,
@@ -15,6 +20,15 @@ pub enum Tag {
     None
 }
 
+/// Token
+///
+/// # members
+/// - NumI32 { num: i32 }   => 整数
+/// - NumF32 { num: f32 }   => 小数
+/// - Word { tag: Tag, lexeme: String } => 語
+///
+/// # derive
+/// - Clone
 #[derive(Clone)]
 pub enum Token {
     NumI32 { num: i32 },
@@ -23,14 +37,35 @@ pub enum Token {
 }
 
 impl Token {
+    /// Token::NumI32構造体を生成して返す
+    ///
+    /// # params
+    /// - num: i32 => 初期化する整数
+    ///
+    /// # returns
+    /// - Token
     pub fn new_numi32(num: i32) -> Token {
         Token::NumI32 { num }
     }
 
+    /// Token::NumF32構造体を生成して返す
+    /// # params
+    /// - num: f32 => 初期化する小数a
+    ///
+    /// # returns
+    /// - Token
     pub fn new_numf32(num: f32) -> Token {
         Token::NumF32 { num }
     }
 
+    /// Token::Word構造体を生成して返す
+    ///
+    /// # params
+    /// - tag: Tag => 関連付けるTag
+    /// - lexeme: &str => 語
+    ///
+    /// # returns
+    /// - Token
     pub fn new_word(tag: Tag, lexeme: &str) -> Token {
         Token::Word { tag, lexeme: String::from_str(lexeme).unwrap() }
     }
