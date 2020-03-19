aa493e5a68c2d7caef9ee8bdc615b2622dae0343
NakagamiYuta <guguru0014@gmail.com>
2020-03-19 23:58:30 +0900

    [add] DefParserにコメントを追加

--- src/chapter2/parser/defparser.rs
+++ src/chapter2/parser/defparser.rs
@@ -2,6 +2,11 @@ use super::Parser;
 use super::super::lexer::Lexer;
 use super::super::symbol::{Symbol, SymbolTable};
 
+/// 定義と使用からなるプログラムをパースする
+///
+/// # members
+/// - lexer: Lexer => 字句解析器
+/// - symboltable: SymbolTable => 記号表
 struct DefParser {
     lexer: Lexer,
     symboltable: SymbolTable
@@ -10,10 +15,25 @@ struct DefParser {
 impl Parser for DefParser {}
 
 impl DefParser {
-    pub fn new(lexer: Lexer) -> DefParser {
+    /// DefParserを生成して返す
+    ///
+    /// # params
+    /// - lexer: Lexer => パースしたいプログラムで初期化された字句解析器
+    ///
+    /// # returns
+    /// DefParser
+    fn new(lexer: Lexer) -> DefParser {
         DefParser { lexer, symboltable: SymbolTable::new() }
     }
 
+    /// DefParserを生成して返す(+記号表セット)
+    ///
+    /// # params
+    /// - lexer: Lexer => パースしたいプログラムで初期化された字句解析器
+    /// - symboltable: SymbolTable => セットする記号表
+    ///
+    /// # returns
+    /// DefParser
     pub fn new_with_symboltable(lexer: Lexer, symboltable: SymbolTable) -> DefParser {
         DefParser { lexer, symboltable }
     }
